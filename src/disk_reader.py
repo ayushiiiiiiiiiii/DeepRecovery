4b11a08b9422eed6ac07f273013c61d8dcf149de
Rupal Patel <rupalp171@gmail.com>
2026-03-18 23:07:58 +0530

    Added disk reader module

--- src/disk_reader.py
+++ src/disk_reader.py
@@ -1,3 +1,4 @@
+
 import os
 
 def scan_disk(image_path, block_size=4096):
@@ -48,3 +49,39 @@ def scan_disk(image_path, block_size=4096):
 if __name__ == "__main__":
     image_path = input("Enter disk image path: ")
     scan_disk(image_path)
+def read_blocks(file_path, block_size=4096):
+    block_number = 1
+
+    try:
+        with open(file_path, "rb") as file:
+
+            while True:
+                block = file.read(block_size)
+
+                if not block:
+                    break
+
+                if block_number<= 5:
+                    print(f"Reading block{block_number},size={len(block)}")
+                  
+                    
+
+               
+
+                yield block_number,block
+
+                block_number += 1
+
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+# Test run
+if __name__ == "__main__":
+    file_path = "input/disk_btrfs.img"
+
+    for block in read_blocks(file_path):
+        pass
+
