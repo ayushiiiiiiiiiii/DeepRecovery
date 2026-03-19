299ec17a4c61a9f29cad7ac631906334fd9fc2f6
Rupal Patel <rupalp171@gmail.com>
2026-03-19 19:30:24 +0530

    Updated disk reader and added gitignore

--- src/disk_reader.py
+++ src/disk_reader.py
@@ -85,3 +85,69 @@ if __name__ == "__main__":
     for block in read_blocks(file_path):
         pass
 
+def read_blocks(file_path, block_size=1024):
+    block_number = 1
+
+    try:
+        with open(file_path, "rb") as file:
+
+            print("Starting disk scan...\n")
+            found_types=set()
+            
+
+            while True:
+                block =file.read(block_size)
+                file.seek(-100,1)
+                if not block:
+                    break
+
+               
+
+
+              # PDF
+                if b"%PDF" in block and "PDF" not in found_types:
+                    print(f"PDF found in block {block_number}")
+                    found_types.add("PDF")
+                
+                # PNG
+                if b"\x89PNG\r\n\x1a\n" in block and "PNG" not in found_types:
+                    print(f"PNG found in block {block_number}")
+                    found_types.add("PNG")
+                
+                # JPG
+                if b"\xff\xd8\xff" in block and "JPG" not in found_types:
+                    print(f"JPG found in block {block_number}")
+                    found_types.add("JPG")
+                
+                # MP3
+                if (b"ID3" in block or b"\xff\xfb" in block) and "MP3" not in found_types:
+                    print(f"MP3 found in block {block_number}")
+                    found_types.add("MP3")
+
+                # MP4
+                if b"ftyp" in block and "MP4" not in found_types:
+                    print(f"MP4 found in block {block_number}")
+                    found_types.add("MP4")
+
+                if block_number<= 5:
+                    print(f"Reading block{block_number},size={len(block)}")
+                  
+
+                yield block_number,block
+
+                block_number += 1
+
+        print("\nScan completed.")
+
+    except FileNotFoundError:
+        print("Error: File not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Test run
+if __name__ == "__main__":
+    file_path = "input/disk_btrfs.img"
+
+    for block in read_blocks(file_path):
+        pass
+
